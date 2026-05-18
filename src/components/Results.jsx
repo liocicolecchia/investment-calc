4c837c779b2229e0c8ddc6b127c15d6af5b1302c
Lio <liocicolecchia@gmail.com>
2026-05-18 21:34:55 +0200

    implementing results table in Results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,42 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
 
-  console.log(resultsData);
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            input.initialInvestment -
+            yearData.annualInvestment * yearData.year;
 
-  return <p>Results....</p>;
+          const totalInvested =
+            input.initialInvestment + yearData.annualInvestment * yearData.year;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Results;
