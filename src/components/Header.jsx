bbd84c9e6e0edd404a4f01262454116a8688e918
Lio <liocicolecchia@gmail.com>
2026-05-18 17:27:18 +0200

    Header logo import fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import logo from "../assets/investment-calculator-logo.png";
+
 function Header() {
   return (
-    <div id="header">
-      <img src="/investment-calculator-logo.png" alt="logo of a bag of money" />
+    <header id="header">
+      <img src={logo} alt="logo of a bag of money" />
       <h1>React Investment Calculator</h1>
-    </div>
+    </header>
   );
 }
 
