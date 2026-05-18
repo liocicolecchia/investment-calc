5dc5e244bda3819cce94f214d7064480b143ed7e
Lio <liocicolecchia@gmail.com>
2026-05-18 16:29:47 +0200

    implementation Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,10 @@
-function Header() {}
+function Header() {
+  return (
+    <div id="header">
+      <img src="/investment-calculator-logo.png" alt="logo of a bag of money" />
+      <h1>React Investment Calculator</h1>
+    </div>
+  );
+}
 
 export default Header;
